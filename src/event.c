0105b30a13cbc0d85799b997b99e1569f6e9e1c4
aboubakr-jelloulat <abubakerjelloulat@gmail.com>
2025-03-10 12:44:19 +0000

    Done

--- src/event.c
+++ src/event.c
@@ -6,7 +6,7 @@
 /*   By: ajelloul <ajelloul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/22 11:45:03 by ajelloul          #+#    #+#             */
-/*   Updated: 2025/03/10 12:43:20 by ajelloul         ###   ########.fr       */
+/*   Updated: 2025/03/10 12:44:10 by ajelloul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,9 +35,9 @@ void mouse_zoom(t_fractol *fractol, int button, int x, int y)
 	new_x = to_complex_plane(y, fractol->oy - (fractol->zoom * 0.128), fractol->oy + (fractol->zoom * 0.128));
 	fractol->ox = new_x;
 	fractol->oy = new_y;
-	if (button == 1)
+	if (button == 4)
 		fractol->zoom += fractol->zoom * 0.1;
-	else if (button == 2)
+	else if (button == 5)
 		fractol->zoom -= fractol->zoom * 0.1;
 }
 
@@ -47,11 +47,11 @@ int mouse_hook(int button, int x, int y, void *param)
 	t_fractol	*fractol;
 
 	fractol = param;
-	// if (button != 1 && button != 2)
-	// {
+	if (button != 1 && button != 2)
+	{
 		mouse_zoom(fractol, button, x, y);
 		ft_draw(*fractol);
-	//}
+	}
 	
 	return (0);
 }
